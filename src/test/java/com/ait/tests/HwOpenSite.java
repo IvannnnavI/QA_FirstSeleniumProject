a619d6a86bc0f5beacda451eb55f5fe864396f64
IvannnnavI <io@email.>
2024-06-11 09:14:43 +0200

    HomeworkQA_1

---
+++ src/test/java/com/ait/tests/HwOpenSite.java
@@ -0,0 +1,23 @@
+package com.ait.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HwOpenSite {
+
+    WebDriver webDriver;
+
+
+    @BeforeMethod
+    public void setUp() {
+        webDriver = new ChromeDriver();
+        webDriver.get("https://demowebshop.tricentis.com/");
+    }
+
+    @Test
+    public void openSite() {
+        System.out.println(" 'Demo Web Shop' is open! ");
+    }
+}
